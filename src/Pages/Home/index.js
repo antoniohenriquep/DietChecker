4ce5a66fccd641cb65e5cc756b8669d1f23826a1
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-04 22:50:52 -0300

    .

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function Home() {
+ return (
+   <View>
+    <Text>Ola</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
