63243e6fe7c01cb5ed850c206b57d11a221b0b40
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-04 22:50:03 -0300

    Resetando o projeto e setando estrutura de naveg

--- App.js
+++ App.js
@@ -1,66 +1,15 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import NumberSelector from './src/Componentes/NumberSelector';
-
-
+import React from 'react';
+import { View, StyleSheet, Text} from 'react-native';
 
 
 export default function App() {
-
-  const [carb, setCarb] = useState(0)
-  var timer = null
-
-function segurar()
-{
-  var i = carb
-  console.log("Segurou")
-  console.log(timer)
-  
-  if(!timer)
-  {
-  timer = setInterval(() =>{ 
-    i++
-    setCarb(i)
-  },250)
-  }else{
-    clearInterval(timer)
-    timer = null
-  }
-  console.log("Saiu")
-}
-
-function apertar()
-{
-  setCarb(carb++)
-}
-
-function soltar()
-{
-  isPressed = false
-  clearInterval(timer)
-  //timer = null
-
-  console.log('Soltou')
-}
-
-
  return (
-    <View style={styles.areaSelectors}>
-      <NumberSelector
-      //aperta = {() =>segurar()}
-      segura = {() =>segurar()}
-      solta = {() =>soltar()}
-      placeholder = {carb.toString()}/>
+   <View>
 
-    </View>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  areaSelectors:{
-    flexDirection:'row',
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-})
+  
+});
