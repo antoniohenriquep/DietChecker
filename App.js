0e2b87213f65a9d5173d070ebd330472309eedb7
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-04 22:58:17 -0300

    Importando stackNavigator

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+
 
 
 export default function App() {
