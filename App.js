ed441557431852db8bdb5669a477ed13bafff4c7
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-05 23:35:40 -0300

    Colocando a navegaçao no projeto

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import Home from './src/Pages/Home';
+import Macros from './src/Pages/Macros';
 
 
+const Stack = createNativeStackNavigator()
 
-export default function App() {
- return (
-   <View>
-
-   </View>
-  );
+export default function App()
+{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component={Home}/>
+        <Stack.Screen name = "Macros" component={Macros}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  
-});
---
+++ src/Pages/Macros/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function Macros() {
+ return (
+   <View>
+    <Text>Ola Macros</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
