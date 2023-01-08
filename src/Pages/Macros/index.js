c75b35c65c12613725cdbe58a1e17f8982224b01
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-07 22:36:09 -0300

    .

--- src/Pages/Macros/index.js
+++ src/Pages/Macros/index.js
@@ -1,6 +1,7 @@
 import Slider from '@react-native-community/slider';
 import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import VerticalSlider from 'rn-vertical-slider';
 
 
 
@@ -11,21 +12,59 @@ export default function Macros() {
   const [carb, setCarb] = useState(50)
   const [prot, setProt] = useState(30)
   const [lip, setLip] = useState(20)
+
+  function verifyMacros()
+  {
+    if((carb + prot + lip) != 100)
+    {
+      alert('A soma bate 100')
+    }
+  }
   
   return (
    <View>
     <Text>Ola Macros</Text>
     <View>
       <Text>Carboidratos: {carb}</Text>
+      <VerticalSlider
+          value={1}
+          disabled={false}
+          min={0}
+          max={100}
+          onChange={(value) => {
+            console.log("CHANGE", value);
+          }}
+          onComplete={(value) => {
+            console.log("COMPLETE", value);
+          }}
+          width={50}
+          height={300}
+          step={1}
+          borderRadius={5}
+          minimumTrackTintColor={"gray"}
+          maximumTrackTintColor={"tomato"}
+          showBallIndicator
+          ballIndicatorColor={"gray"}
+          ballIndicatorTextColor={"white"}
+        />
+      <Text>Proteínas: {prot}</Text>
       <Slider
-      value={carb} 
+      value={prot} 
       minimumValue={5} 
       maximumValue ={90}
-      onValueChange ={(value) => {setCarb(Math.floor(value))}}/>
-      <Text>Proteínas</Text>
-      <Slider minimumValue={5} maximumValue ={90}/>
-      <Text>Gorduras</Text>
-      <Slider minimumValue={5} maximumValue ={90}/>
+      onValueChange ={(value) => {setProt(Math.floor(value))}}/>
+      <Text>Gorduras: {lip}</Text>
+      <Slider
+      value={lip} 
+      minimumValue={5} 
+      maximumValue ={90}
+      onValueChange ={(value) => {setLip(Math.floor(value))}}/>
+
+      <Button
+      title='Concluir'
+      onPress={() =>{
+        verifyMacros()
+      }}/>
     </View>
    </View>
   );
